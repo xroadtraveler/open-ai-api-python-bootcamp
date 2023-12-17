c59807ac3244550d25b4a726ffbbc66cb3273e44
xroadtraveler <xroadtraveler@gmail.com>
2023-12-17 12:35:15 -0800

    add 'st_callbacks.py' file

---
+++ _Python_Apps/Section-14__Appendix__Streamlit/Section_14_Streamlit_Project/st_callbacks.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+# CALLBACKS
+st.subheader('Distance Converter')
+
+
+def miles_to_km():
+    st.session_state.km = st.session_state.miles * 1.609
+
+def km_to_miles():
+    st.session_state.miles = st.session_state.km * 0.621
+
+
+col1, buff, col2 = st.columns([2, 1, 2])
+with col1:
+    miles = st.number_input('Miles:', key='miles', on_change=miles_to_km)
+
+with col2:
+    km = st.number_input('Km:', key='km', on_change=km_to_miles)
